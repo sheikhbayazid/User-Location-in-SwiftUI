b5e12e65e384ee5d0d8205e4dac1d4596c3b7c5e
sheikhbayazid <44797746+sheikhbayazid@users.noreply.github.com>
2020-07-22 00:34:25 +0600

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,84 @@
+//
+//  ContentView.swift
+//  LocationDemo
+//
+//  Created by Sheikh Bayazid on 7/19/20.
+//  Copyright © 2020 Sheikh Bayazid. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct ContentView: View {
+    @ObservedObject var location = LocationManager()
+
+    
+    var lat: String{
+        return "\(location.lastKnownLocation?.coordinate.latitude ?? 0.0)"
+    }
+    
+    var lon: String{
+        return "\(location.lastKnownLocation?.coordinate.longitude ?? 0.0)"
+    }
+    
+    init() {
+        self.location.startUpdating()
+    }
+    
+    var symbol = "bolt.heart.fill"
+    
+    
+    
+    var body: some View {
+        ZStack{
+            Color(red: 224/255, green: 229/255, blue: 236/255)
+            VStack {
+                Text("Location breakdown")
+                Text("Latitude: \(lat)")
+                Text("Longitude: \(lon)")
+            }
+            .padding()
+            .padding()
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(radius: 10)
+        }.edgesIgnoringSafeArea(.all)
+    }
+}
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
+
+
+
+//
+//init() {
+//
+//    self.manager.delegate = self.managerDelegate
+//    do {
+//        try self.manager.requestAlwaysAuthorization()
+//
+//        //            if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+//        //                CLLocationManager.authorizationStatus() == .authorizedAlways) {
+//        //                currentLoc = locationManager.location
+//        //                latitude = currentLoc.coordinate.latitude
+//        //                longitude = currentLoc.coordinate.longitude
+//        //
+//        //
+//        //            }
+//
+//    }
+//    catch {
+//        print(error.localizedDescription)
+//        self.manager.requestAlwaysAuthorization()
+//    }
+//}
